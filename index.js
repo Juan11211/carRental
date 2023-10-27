17f18dcb72659f248407f212966f71245039cff9
Juan11211 <avilesjuan628@gmail.com>
2023-10-27 13:36:20 -0400

    working on js file

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict"
+
+window.onload = function(){
+
+    let estimateTotalCost = document.getElementById("estimateTotalCost");
+    estimateTotalCost.onclick = estimateTotalCostClicked;
+}
+
+function estimateTotalCost(){
+
+    let pickUpDate = document.getElementById("pickUpDate");
+    let numberOfDays = document.getElementById("numberOfDays");
+    let tollTag = document.getElementById("tollTag");
+    let 
+
+}
